e20af2305ce8175941209cccc43647270fbebd90
Thomas Bak <tombak98@gmail.com>
2022-08-12 16:36:02 -0400

    finished everything except pug routes

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,10 @@ const Pug = require('./pug.model')
 const Coffee = require('./coffee.model')
 
 // VVV assign relations below VVV //
+Pug.belongsTo(Coffee, {as: 'favoriteCoffee'})
+Coffee.hasMany(Pug, {foreignKey: 'favoriteCoffeeId'})
 
+Pug.hasMany(Pug, {as: 'friends'})
 
 // ^^^ assign relations above ^^^ //
 
--- server/models/pug.model.js
+++ server/models/pug.model.js
@@ -4,6 +4,60 @@ const Coffee = require('./coffee.model')
 
 const Pug = db.define('pugs', {
   // your code here
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  biography: {
+    type: Sequelize.TEXT
+  }
+})
+
+Pug.prototype.isPuppy = function() {
+  if (this.age >= 1) {
+    return false
+  } else {
+    return true
+  }
+}
+
+Pug.prototype.shortBio = function() {
+  let punct = ".?!"
+  for (let i = 0; i < this.biography.length; i++) {
+    if (punct.includes(this.biography[i])) {
+      return this.biography.slice(0,i)
+    }
+  }
+}
+
+Pug.findByCoffee = async function(str) {
+  let favCoffee = await Coffee.findOne({
+    where: {
+      name: str
+    }
+  })
+  let pugs = Pug.findAll({
+    where: {
+      favoriteCoffeeId: favCoffee.id
+    },
+    include: {
+      model: Coffee,
+      as: 'favoriteCoffee'
+    }
+  })
+  return pugs
+}
+
+Pug.beforeCreate((pug) => {
+  pug.name = pug.name.charAt(0).toUpperCase() + pug.name.slice(1)
+})
+
+Pug.beforeUpdate((pug) => {
+  pug.name = pug.name.charAt(0).toUpperCase() + pug.name.slice(1)
 })
 
 module.exports = Pug
